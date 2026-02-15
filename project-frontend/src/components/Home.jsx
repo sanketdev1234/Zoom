636a9663e25ecf15adac16711d69a09dfe6c824e
sanketdev1234 <codetech28@gmail.com>
2026-02-15 18:42:07 +0530

    done

--- project-frontend/src/components/Home.jsx
+++ project-frontend/src/components/Home.jsx
@@ -1,54 +1,61 @@
-
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-import CreateMeeting from "./Meet/CreateMeeting"
-import Login from "./Auth/Login"
-import Signup from "./Auth/Signup"
-import Showmeetdetail from "./Meet/Showmeetdetail"
-import Userdashboard from "./Userdashboard"
-import ScoketChat from "./Chat/ScoketChat"
-// import VideoChat from "./VideoChat"
-import Contactsanket from "./Contactsanket"
+import Sidebar from "./Sidebar";
+import CreateMeeting from "./Meet/CreateMeeting";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import Showmeetdetail from "./Meet/Showmeetdetail";
+import Userdashboard from "./Userdashboard";
+import ScoketChat from "./Chat/ScoketChat";
+import Contactsanket from "./Contactsanket";
 import Joinmeeting from "./Meet/Joinmeet";
-import NotFound from "./Notfound"
+import NotFound from "./Notfound";
 import ProfileCreation from "./profile/profilecreate";
 import DisplayProfile from "./profile/profileview";
 import UpdateProfile from "./profile/updateprofile";
-import CreatePost from "./post/createpost"
+import CreatePost from "./post/createpost";
 import LandingPage from "./Landingpage";
 import SeeAllPosts from "./post/seeallpost";
-import EditPost from "./post/editpost"
+import EditPost from "./post/editpost";
 import SeeAllPostsUser from "./post/seespecificpost";
-import ShowmyConnections from "./connections/showmyconnections"
-import Showincomingrequest from "./connections/incomingrequests"
-function PageWrapper({ children }) {
-    return (
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.3 }}
-      >
-        {children}
-      </motion.div>
-    );
-  }
+import ShowmyConnections from "./connections/showmyconnections";
+import Showincomingrequest from "./connections/incomingrequests";
 
-  function AnimatedRoutes() {
-    const location = useLocation();
-    return (
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  
+  // Routes where sidebar should NOT be shown
+  const noSidebarRoutes = ['/', '/landingpage', '/login', '/signup'];
+  const showSidebar = !noSidebarRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {showSidebar && <Sidebar />}
+      
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageWrapper><LandingPage/></PageWrapper>} />
-        <Route path="/landingpage" element={<PageWrapper><LandingPage/></PageWrapper>} />
-          <Route path="/dashboard" element={<PageWrapper><Userdashboard/></PageWrapper>} />
-          <Route path="/newmeet" element={<PageWrapper><CreateMeeting/></PageWrapper>} />
-          <Route path="/joinmeet" element={<PageWrapper><Joinmeeting/></PageWrapper>} />
+          <Route path="/" element={<PageWrapper><LandingPage /></PageWrapper>} />
+          <Route path="/landingpage" element={<PageWrapper><LandingPage /></PageWrapper>} />
+          <Route path="/dashboard" element={<PageWrapper><Userdashboard /></PageWrapper>} />
+          <Route path="/newmeet" element={<PageWrapper><CreateMeeting /></PageWrapper>} />
+          <Route path="/joinmeet" element={<PageWrapper><Joinmeeting /></PageWrapper>} />
           <Route path="/signup" element={<PageWrapper><Signup /></PageWrapper>} />
           <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
           <Route path="/meet/:id/detail" element={<PageWrapper><Showmeetdetail /></PageWrapper>} />
           <Route path="/ongoingmeet/:meetid/:joinid" element={<PageWrapper><ScoketChat /></PageWrapper>} />
-          {/* <Route path="/videomeet/:meetid/:joinid" element={<PageWrapper><VideoChat /></PageWrapper>} /> */}
           <Route path="/contact" element={<PageWrapper><Contactsanket /></PageWrapper>} />
           <Route path="/createprofile" element={<PageWrapper><ProfileCreation /></PageWrapper>} />
           <Route path="/getprofile/:wantid" element={<PageWrapper><DisplayProfile /></PageWrapper>} />
@@ -60,16 +67,18 @@ function PageWrapper({ children }) {
           <Route path="/myconnections" element={<PageWrapper><ShowmyConnections /></PageWrapper>} />
           <Route path="/incomingrequests" element={<PageWrapper><Showincomingrequest /></PageWrapper>} />
           <Route path="/*" element={<PageWrapper><NotFound /></PageWrapper>} />
-  
         </Routes>
       </AnimatePresence>
-    );
-  }
-function Home(){
-  return (
-  <div>
-    <AnimatedRoutes/>
-  </div>
-  )
+    </>
+  );
 }
+
+function Home() {
+  return (
+    <div>
+      <AnimatedRoutes />
+    </div>
+  );
+}
+
 export default Home;
